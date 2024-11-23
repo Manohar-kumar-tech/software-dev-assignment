006829d1687e4ff245982b09363e455c777bfe02
Manohar <manohar.kumar.cse@gmail.com>
2024-11-23 14:28:39 +0530

    it didn't worked at first.

---
+++ DS/indexInTuple.py
@@ -0,0 +1,13 @@
+def findIndex(element, tuple):
+    for i in range(len(tuple)):
+        if tuple[i] == element:
+            return i
+    return -1
+    
+    
+
+t = (1, 2, 10, 20, 30, 40)
+target =30 
+
+result = findIndex(target, t)
+print(f"The give element fond at index {result}")
---
+++ DS/mergeDict.py
@@ -0,0 +1,21 @@
+# merging two dictionary
+# def merge(dic1, dic2):
+#     for key, value in dic2.items():
+#         dic1[key] = value
+#     return dic1
+
+# d ={"name": "manohar", "middle": "kumar", "last": "Paswan"}
+# d2 = {"Education": "Graduation", "Status": "unemployed"}
+
+# result = merge(d, d2)
+# print(result)
+
+
+
+
+# second programm convert list to tuple
+l = [1, 2, 3, "mano", "hello"]
+
+tu = tuple(l)
+print(type(tu))
+print(tu)
